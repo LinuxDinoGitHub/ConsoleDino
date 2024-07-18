73825b3517ac01b860499e39aac2463c1c02c45f
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-18 22:27:32 +0800

    Implemented jumping mechanism

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 void printCanvas(char **canvas) {
-    for (int i = 0; i < 14; i++) {
+    for (int i = 0; i < 18; i++) {
         for (int j = 0; j < 80; j++) {
             cout << canvas[i][j];
         }
@@ -48,7 +48,7 @@ void drawDino(int posx, int posy, char **canvas, int dino){ //posx and pos y ref
     }
     for(int i = 0; i<5; i++){
         for(int j = 0; j<pDino[i].length(); j++){
-            if(i+posx >= 0 && i+posx < 11 && j+posy >= 0 && j+posy < 80){
+            if(i+posx >= 0 && i+posx < 13 && j+posy >= 0 && j+posy < 80){
                 canvas[i+posx][j+posy] = pDino[i][j];
             }  
         }
@@ -60,11 +60,11 @@ void clearDino(char **canvas, int lastposx, int lastposy){ //top dino length is 
     {"              / ._|",
     "     _.----._/ /",
     "    /         /",
-    " __/ (  | ( \\",
-    "/__.-'\\_|--\\_\\"};
+    " __/ (  | (  \\|",
+    "/__.-'\\_|--|_ |"};
     for (int i = 0; i < 5; i++){
         for (int j = 0; j<dinoSize[i].length(); j++){
-            canvas[i+lastposx][j+lastposy] = ' ';
+            canvas[i+lastposy][j+lastposx] = ' '; //0, 8 current
         }
     }
 }
@@ -88,13 +88,13 @@ void startGame(){
     ShowConsoleCursor(false);
     bool gameOver = false;
     int dinostate = 0;
-    char **canvas = new char*[14];
-    for (int i = 0; i < 14; i++) {
+    char **canvas = new char*[18];
+    for (int i = 0; i < 18; i++) {
         canvas[i] = new char[80];
     }
-    for (int i = 0; i < 14; i++) {
+    for (int i = 0; i < 18; i++) {
         for (int j = 0; j < 80; j++) {
-            if(i == 11){
+            if(i == 13){
                 canvas[i][j] = '_';
             }
             else{
@@ -102,16 +102,32 @@ void startGame(){
             }
         }
     }
-    /*
-    while (dinostate<30){
-        clearDino(canvas, 6,0);
-        drawDino(6,0,canvas, dinostate%3); //y, x, default 6, 0
+    int dinoVelocity = 0;
+    int timePassed = 0;
+    int posx = 0;
+    int posy = 8;
+    while(!gameOver){ //Runs every frame
+        clearDino(canvas, posx, posy);
+        if(GetKeyState(VK_SPACE) & 0x8000 && dinoVelocity == 0){
+            dinoVelocity = 6;
+        }
+        if(dinoVelocity != 0){
+            posy -= 1;
+            dinoVelocity -= 1;
+            dinostate = 0;
+        }
+        else{
+            if(posy < 8){
+                posy += 1;
+            }
+        }
+        drawDino(posy,posx,canvas, dinostate%3); //y, x, default 6, 0
         dinostate++;
-        printCanvas(canvas);
         this_thread::sleep_for(chrono::milliseconds(200));
         system("cls");
-    }*/
-    for(int i = 0; i < 14; i++) {
+        printCanvas(canvas);
+    }
+    for(int i = 0; i < 18; i++) {
         delete[] canvas[i];
     }
     delete[] canvas;
