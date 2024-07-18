b4ddc4bafae3042c58e1bf90851c5eb15e6ef0c0
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-18 16:25:41 +0800

    animation done for dino

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <unistd.h>
+#include <time.h>
 
 using namespace std;
 
@@ -12,20 +15,60 @@ void printCanvas(char **canvas) {
     }
 }
 
-void drawDino(int posx, int posy, char **canvas){ //posx and pos y refers to bottom left of screen
-    const string dino[5] = 
-    {"              / _",
+void drawDino(int posx, int posy, char **canvas, int dino){ //posx and pos y refers to bottom left of screen
+    const string dinoJump[5] = 
+    {"              / ._|", //19 chars
     "     _.----._/ /",
     "    /         /",
     " __/ (  | (  |",
     "/__.-'|_|--|_|"};
-    for(int i = 0; i<=5; i++){
-        for(int j = 0; j<dino[i].length(); j++){
-            if(i+posx >= 0 && i+posx < 80 && j+posy >= 0 && j+posy < 11){
-                canvas[i+posx][j+posy] = dino[i][j];
+    const string dinoWalk1[5] = 
+    {"              / ._|",
+    "     _.----._/ /",
+    "    /         /",
+    " __/ (  |  (  |",
+    "/__.-'/_/---/_/"};
+    const string dinoWalk2[5] = 
+    {"              / ._|",
+    "     _.----._/ /",
+    "    /         /",
+    " __/ (  | ( \\",
+    "/__.-'\\_|--\\_\\"};
+    const string *pDino;
+    if(dino == 0){
+        pDino = dinoJump;
+    }
+    else if (dino == 1){
+        pDino = dinoWalk1;
+    }
+    else{
+        pDino = dinoWalk2;
+    }
+    for(int i = 0; i<5; i++){
+        for(int j = 0; j<pDino[i].length(); j++){
+            if(i+posx >= 0 && i+posx < 11 && j+posy >= 0 && j+posy < 80){
+                canvas[i+posx][j+posy] = pDino[i][j];
             }  
         }
     }
+}
+
+void clearDino(char **canvas, int lastposx, int lastposy){ //top dino length is 19
+    const string dinoSize[5] = 
+    {"              / ._|",
+    "     _.----._/ /",
+    "    /         /",
+    " __/ (  | ( \\",
+    "/__.-'\\_|--\\_\\"};
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j<dinoSize[i].length(); j++){
+            canvas[i+lastposx][j+lastposy] = ' ';
+        }
+    }
+}
+
+int touching(char **canvas){
+    return 0;
 }
 
 void startGame(){
@@ -43,8 +86,16 @@ void startGame(){
             }
         }
     }
-    drawDino(2,8,canvas);
-    printCanvas(canvas);
+    int dinostate = 0;
+    while (dinostate<30){
+        clearDino(canvas, 6,0);
+        drawDino(6,0,canvas, dinostate%3); //y, x, default 6, 0
+        dinostate++;
+        printCanvas(canvas);
+        sleep(0.5);
+        system("cls");
+        sleep(0.1);
+    }
     for (int i = 0; i < 14; i++) {
         delete[] canvas[i];
     }
