6074e6886519f0958bfdfd9c8e2eec198451f2cf
LinuxDinoGitHub <95269147+LinuxDinoGitHub@users.noreply.github.com>
2024-07-18 13:08:01 +0800

    Initialised canvas

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    cout << "Hello world";
+    string canvas[50][13];
+    const string dino[5] = 
+    {"              / _",
+    "     _.----._/ /",
+    "    /         /",
+    " __/ (  | (  |",
+    "/__.-'|_|--|_|"};
+    for(string x : dino){
+        cout << x << endl;
+    }
+    cout << canvas;
     return 0;
+}
+void printCanvas(const vector<vector<string>>& canvas) {
+    for (const auto& row : canvas) {
+        for (const auto& cell : row) {
+            cout << cell << " ";
+        }
+        cout << endl;
+    }
+}
+char* initCanvas(){
+    static char canvas[13][50];
+    for (int i=0; i<14; i++) {
+        for (int j=0; j<50; j++) {
+            canvas[i][j] = " ";
+        }
+    }
+    return canvas; //returns address of the array
 }
